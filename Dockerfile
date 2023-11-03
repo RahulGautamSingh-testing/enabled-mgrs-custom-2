d8b8ab5fb0e391548855f8a109ccadf553af504a
RahulGautamSingh-testing <99875673+RahulGautamSingh-testing@users.noreply.github.com>
2023-11-03 18:52:46 +0545

    Update and rename Dockerfile to index.ts

--- Dockerfile
+++
@@ -1,5 +0,0 @@
-# renovate: datasource=github-releases packageName=containerbase/python-prebuild
-RUN install-tool python 3.11.0
-
-# renovate: datasource=npm
-RUN install-tool corepack 0.20.0
---
+++ index.ts
@@ -0,0 +1,8 @@
+  {
+    name: 'dockerSidecarImage',
+    description:
+      'Change this value to override the default Renovate sidecar image.',
+    type: 'string',
+    default: 'ghcr.io/containerbase/sidecar:9.23.8',
+    globalOnly: true,
+  },
